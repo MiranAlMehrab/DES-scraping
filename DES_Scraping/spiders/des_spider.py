b8a77d0b5ce2662cc8942bcbae68034d7724966d
MiranAlMehrab <bsse0931@iit.du.ac.bd>
2021-03-07 12:16:55 +0600

    code refactored and pyc files ignored

--- DES_Scraping/spiders/des_spider.py
+++ DES_Scraping/spiders/des_spider.py
@@ -4,7 +4,6 @@ from DES_Scraping.items import DESItem
 from scrapy.loader import ItemLoader 
 
 
-
 class DesSpider(scrapy.Spider):
     name = "des"
 
@@ -14,11 +13,7 @@ class DesSpider(scrapy.Spider):
 
     def parse(self, response):
         
-        requested_page = response.url.split("/")[-2]
-
-        # print('debug point: ------------------------')
-        # print(response.xpath("/html/body/div[2]/section/div/div[3]/div[1]/div[2]/div[1]/div[1]/table/tbody[1]/tr.[*/2]"))
-        
+        requested_page = response.url.split("/")[-2]        
         # table = response.xpath(".//table[@class = 'table table-bordered background-white shares-table fixedHeader']/*")
         # print('-----------------len-----------------')
         # print(len(table))
@@ -27,48 +22,43 @@ class DesSpider(scrapy.Spider):
         # for tbody in table:
             
         #     tbody_counter += 1
+
+        table_rows = response.xpath("//table[@class = 'table table-bordered background-white shares-table fixedHeader']/*")
         
-
-
-
-
-        trs = response.xpath("//table[@class = 'table table-bordered background-white shares-table fixedHeader']/*")
-        
-        for i in range(2, len(trs)):
+        for i in range(2, len(table_rows)):
 
             print('----------------------------------- look here -----------------------------------: '+str(i))
-            print(trs[i].xpath('./*'))
+            print(table_rows[i].xpath('./*'))
             
             # if i == 1:
-            #     list_td = trs[i].xpath('./*') 
-            #     print(len(trs[i].xpath('./*')))
+            #     row_tds = table_rows[i].xpath('./*') 
+            #     print(len(table_rows[i].xpath('./*')))
             
-             
-            list_td = trs[i].xpath('./*')
-            print(len(trs[i].xpath('./*')))
+            row_tds = table_rows[i].xpath('./*')
+            print(len(table_rows[i].xpath('./*')))
 
             try:
                 item = DESItem()
 
-                item['trading_code'] = (list_td[1].xpath(".//a/text()").extract_first()).strip()
-                item['last_traded_price'] = list_td[2].xpath("text()").extract_first()
-                item['high'] = list_td[3].xpath("text()").extract_first()
-                item['low'] = list_td[4].xpath("text()").extract_first()
-                # item['opening_price'] = 
-                item['closing_price'] = list_td[5].xpath("text()").extract_first()
-                item['yesterdays_closing_price'] = list_td[6].xpath("text()").extract_first()
-                item['change'] = list_td[7].xpath("text()").extract_first()
-                item['trade'] = list_td[8].xpath("text()").extract_first()
-                item['value_mn'] = list_td[9].xpath("text()").extract_first()
-                item['volume'] = list_td[10].xpath("text()").extract_first()
+                item['trading_code'] = (row_tds[1].xpath(".//a/text()").extract_first()).strip()
+                item['last_traded_price'] = row_tds[2].xpath("text()").extract_first()
+                item['high'] = row_tds[3].xpath("text()").extract_first()
+                item['low'] = row_tds[4].xpath("text()").extract_first()
+                item['closing_price'] = row_tds[5].xpath("text()").extract_first()
+                item['yesterdays_closing_price'] = row_tds[6].xpath("text()").extract_first()
+                item['change'] = row_tds[7].xpath("text()").extract_first()
+                item['trade'] = row_tds[8].xpath("text()").extract_first()
+                item['value_mn'] = row_tds[9].xpath("text()").extract_first()
+                item['volume'] = row_tds[10].xpath("text()").extract_first()
 
                 yield item
 
             except:
-                print('exception khaise')
-                raise Exception("exception khaise")
+                raise Exception("exception occurred")
                 print("Oops!", sys.exc_info()[0], "occurred.")
         
-            # yield {
-            #     "data": response.xpath("//tr").extract_first()
-            # }
+            
+    def fetch_table_row_with_whitespace():
+        yield {
+                "data": response.xpath("//tr").extract_first()
+            }
